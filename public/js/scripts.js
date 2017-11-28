2a3775b44dec6c572e2057232e1384312f88b015
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-11-28 15:37:05 -0700

    Clean up comments and white space, org FE functions

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,7 @@
+$(document).ready(() => {
+  updateRandomColors();
+});
+
 const addProject = () => {
   let newProjectName = $('#new-project').val();
   $('.project-directory').prepend(`
@@ -10,31 +14,6 @@ const addProject = () => {
   $('#new-project').val('');
 };
 
-const generateColors = () => {
-  //call a helper to determine how many colors need to be generated
-  //generate that many Colors
-  const characters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += characters[Math.floor(Math.random() * 16)];
-  };
-  return color;
-};
-
-const savePalette = () => {
-  let project = $('#project-menu').find(':selected').text();
-  let paletteName = $('#new-palette').val();
-  //need to grab the HEX codes tha belong with this palette
-  //append the name and baby-palette to the UL on left file tree
-  //need to interact with BE here - send this palette to DB
-  console.log(project, paletteName);
-  $('#new-palette').val('');
-}
-
-$(document).ready(() => {
-  updateRandomColors();
-});
-
 const updateRandomColors = () => {
   for (var i = 0; i < 6; i++) {
 
@@ -46,6 +25,34 @@ const updateRandomColors = () => {
  };
 }
 
+const generateColors = () => {
+  //call a helper to determine how many colors need to be generated
+  //generate that many colors
+  const characters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += characters[Math.floor(Math.random() * 16)];
+  };
+  return color;
+};
+
+const toggleFavorite = () => {
+    //let newFavorite =
+    //grab hex code of the color that was clicked on
+};
+
+const savePalette = () => {
+  let project = $('#project-menu').find(':selected').text();
+  let paletteName = $('#new-palette').val();
+  //need to grab the HEX codes tha belong with this palette
+  //append the name and baby-palette to the UL
+  //need to interact with BE here - send this palette to DB
+  console.log(project, paletteName);
+  $('#new-palette').val('');
+}
+
 $('#new-project-btn').on('click', addProject);
 $('.generate-btn').on('click', updateRandomColors);
 $('#new-palette-btn').on('click', savePalette);
+$('.empty-heart-icon').on('click', toggleFavorite);
+$('.full-heart-icon').on('click', toggleFavorite);
