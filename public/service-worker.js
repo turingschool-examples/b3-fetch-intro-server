d22c34d6e1f9dfd786175fcedcb882432074ab67
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-12-07 06:56:51 -0700

    Add manifest and service worker

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('assets-v1')
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          '/js/scripts.js',
+          '/css/styles.css',
+          '/lib/jquery-3.2.1.js',
+          '/img/color-palette.svg',
+          '/img/delete-icon.svg',
+          '/img/padlock.svg',
+          '/img/unlock.svg',
+        ]);
+      })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// this.addEventListener('activate', event => {
+//   let cacheKeep = ['assets-v1'];
+//   event.waitUntil(
+//     caches.keys().then(keys => {
+//       return Promise.all(keys.map(key => {
+//         if (cacheKeep.indexOf(key) === -1) {
+//           return caches.delete(key);
+//         }
+//       }));
+//     })
+//   );
+// });
--- server.js
+++ server.js
@@ -11,15 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + '/public')));
 
 const requireHTTPS = (request, response, next) => {
-  if (request.header('x-forwarded-proto') !== 'https') {
+  if (request.header('x-forwarded-proto') != 'https') {
     return response.redirect(`https://${request.header('host')}${request.url}`);
   }
   next();
 };
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(requireHTTPS);
-}
+if (process.env.NODE_ENV === 'production') { app.use(requireHTTPS); }
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
