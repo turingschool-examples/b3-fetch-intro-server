dbe5dc2f07cbae26987fb396317340ec5783529b
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-02-20 13:18:06 -0700

    Update CORS

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname + '/public')));
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
