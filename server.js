bb66dace024ab79aedb00255aa97fdefefb33540
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-02-20 13:24:32 -0700

    Add cors methods

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use(express.static(path.join(__dirname + '/public')));
 app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
